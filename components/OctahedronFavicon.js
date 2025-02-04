607a3eccabd846935b53a73a3fd0e9eaf9a55ede
MichielMAnalytics <135761097+MichielMAnalytics@users.noreply.github.com>
2025-02-04 13:26:57 +0100

    favicon add

---
+++ components/OctahedronFavicon.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const OctahedronFavicon = () => (
+  <svg
+    width="32"
+    height="32"
+    viewBox="0 0 400 400"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M200 40L40 200L200 360L360 200L200 40Z"
+      fill="#F4E409"
+      fillOpacity="0.7"
+    />
+    <path
+      d="M200 40L40 200L200 360L360 200L200 40Z"
+      stroke="#F4E409"
+      strokeWidth="2"
+      strokeOpacity="0.3"
+    />
+  </svg>
+);
+
+export default OctahedronFavicon; 
